b8512598c6553dd1f4990c4bfe0a6ff6c1b750b8
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-03 16:15:24 +0530

    update migration update id type write logic to save data into database

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ContactRequest;
 use DB;
+use App\Models\ContactUs;
 
 class ContactController extends Controller
 {
@@ -14,6 +15,22 @@ class ContactController extends Controller
 
     public function store(ContactRequest $request)
     {
-        dd($request->all());
+        try {
+            $sendData = [
+                "name" => $request->name,
+                "email" => $request->email,
+                "phone" => (int) $request->phone
+            ];
+            DB::beginTransaction();
+            $isCreate = ContactUs::create($sendData);
+            DB::commit();
+            if ($isCreate) {
+                return redirect()->back()->with("success", "Your message has been sent successfully.");
+            }
+        } catch (\Exception $e) {
+            DB::rollBack();
+            // return redirect()->back()->with("error", $e->getMessage());
+            echo $e->getMessage();
+        }
     }
 }
--- database/migrations/2024_10_03_102350_create_contact_us_table.php
+++ database/migrations/2024_10_03_102350_create_contact_us_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('contact_us', function (Blueprint $table) {
-            $table->id();
+            $table->uuid("id")->primary();
             $table->string('name');
             $table->string('email');
             $table->integer('phone');
