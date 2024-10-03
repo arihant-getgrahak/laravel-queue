677401feffde641ca15155c678dcf4c2a578b432
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-03 15:54:30 +0530

    create model file and migration file for contactUs form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
+use DB;
 
 class ContactController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view("welcome");
     }
 
-    public function store(){}
+    public function store(ContactRequest $request)
+    {
+    }
 }
