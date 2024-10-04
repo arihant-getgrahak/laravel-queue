f9ba299c50e7c8115222084346b8dfdbe8b5cb66
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-04 21:27:26 +0530

    add func to store data to db

--- app/Http/Controllers/FileManagerController.php
+++ app/Http/Controllers/FileManagerController.php
@@ -3,10 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Models\FileManager;
+use Storage;
 
 class FileManagerController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('welcome');
+    }
+    public function store(Request $request)
+    {
+        try {
+            $sendData = [
+                "name" => $request->name,
+                "email" => $request->email,
+                "phone" => (int) $request->phone,
+                "image" => $request->file('image') ? $this->uploadImage($request->file('image')) : null,
+            ];
+            DB::beginTransaction();
+
+            // dd($sendData);
+
+            $isCreate = FileManager::create($sendData);
+
+            DB::commit();
+
+            if ($isCreate) {
+                return redirect()->back()->with("success", "Your message has been sent successfully.");
+            }
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->back()->with("error", $e->getMessage());
+        }
+    }
+
+    protected function uploadImage($file)
+    {
+        $uploadFolder = 'contact-us';
+        $image = $file;
+        $image_uploaded_path = $image->store($uploadFolder, 'public');
+        $uploadedImageUrl = Storage::disk('public')->url($image_uploaded_path);
+
+        return $uploadedImageUrl;
     }
 }
