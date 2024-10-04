2a5175a8d3db6bbaa91ddf2101b016714dee70a3
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-04 21:28:47 +0530

    add field name and type in migration table

--- database/migrations/2024_10_04_152926_create_file_managers_table.php
+++ database/migrations/2024_10_04_152926_create_file_managers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('file_managers', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
             $table->timestamps();
         });
     }
