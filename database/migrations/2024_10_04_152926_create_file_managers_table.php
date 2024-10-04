f41f9b809db2a67d3ef553f3bed2dedb504a167a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-04 21:54:44 +0530

    update id type to uuid

--- database/migrations/2024_10_04_152926_create_file_managers_table.php
+++ database/migrations/2024_10_04_152926_create_file_managers_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('file_managers', function (Blueprint $table) {
-            $table->id();
+            $table->uuid("id")->primary();
             $table->string('image');
             $table->timestamps();
         });
