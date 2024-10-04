4826f768016164d5601ef6a6342df8d55e3836c9
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-04 21:13:11 +0530

    update routes for contact and file manager

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\FileManagerController;
 
-Route::get('/', [ContactController::class, 'index']);
+
+Route::get('/', [FileManagerController::class, 'index']);
+
+Route::post('/store', [FileManagerController::class, 'store']);
+
+Route::get('/store', [FileManagerController::class, 'store']);
+
+Route::get('/contact', [ContactController::class, 'index']);
 
 Route::post('/contact', [ContactController::class, 'store']);
 
