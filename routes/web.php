e2ce1ac875a60423a59460f6c936ac93338efddb
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-04 21:41:16 +0530

    remove extra route

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,9 @@ Route::get('/', [FileManagerController::class, 'index']);
 
 Route::post('/store', [FileManagerController::class, 'store']);
 
-Route::get('/store', [FileManagerController::class, 'store']);
 
 Route::get('/contact-us', [ContactController::class, 'index']);
 
 Route::post('/contact', [ContactController::class, 'store']);
 
-Route::get('/contact', [ContactController::class, 'store']);
 
